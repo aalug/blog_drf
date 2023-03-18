2610f7ed5753103d36676a7605211019ba2712cc
Adam Gulczynski <adamg@example.com>
2023-03-18 17:14:09 +0100

    #9 completed. Post related models created.

--- core/models.py
+++ core/models.py
@@ -1,8 +1,20 @@
 """
 Database models.
 """
+import uuid
+import os
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings
+from django.utils.text import slugify
+
+
+def image_file_path(instance, filename):
+    """Generate file path for new image"""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+    return os.path.join('uploads', 'post', filename)
 
 
 class UserManager(BaseUserManager):
@@ -34,6 +46,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255, blank=True, null=True)
     date_of_birth = models.DateField()
+    points = models.PositiveIntegerField(default=0)
+    profile_image = models.ImageField(upload_to=image_file_path, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -41,3 +55,80 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Post(models.Model):
+    """Post table."""
+    title = models.CharField(max_length=240, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    description = models.CharField(max_length=255)
+    body = models.TextField()
+    cover_image = models.ImageField(upload_to=image_file_path, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at', '-updated_at']
+
+    @property
+    def comments(self):
+        """Return comments for this post."""
+        return Comment.objects.filter(post=self)
+
+    @property
+    def images(self):
+        """Return images for this post."""
+        return PostImage.objects.filter(post=self)
+
+    def save(self, *args, **kwargs):
+        """Create a slug for this post before saving it."""
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+
+class PostImage(models.Model):
+    """Image for a Post."""
+    title = models.CharField(max_length=100)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=image_file_path)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    """Comment to a post."""
+    text = models.TextField(max_length=1000)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}: {self.text[:20]}'
+
+    class Meta:
+        ordering = ['-created_at', '-updated_at']
+
+
+class Tag(models.Model):
+    """Tag for filtering posts."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -3,11 +3,16 @@ Test fpr models.
 """
 import datetime
 
+from unittest.mock import patch
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.utils.text import slugify
+
+from core.models import Post, Comment, PostImage, Tag, image_file_path
 
 
-def create_user(user_details: dict[str, str]):
+def create_user(user_details: dict[str, str] = None):
     """Create and return a new user."""
     payload = {
         'email': 'user@example.com',
@@ -15,12 +20,27 @@ def create_user(user_details: dict[str, str]):
         'first_name': 'First',
         'date_of_birth': '1950-10-10'
     }
-    payload.update(user_details)
+    if user_details is not None:
+        payload.update(user_details)
     return get_user_model().objects.create_user(payload)
 
 
-class ModelTest(TestCase):
-    """Test models."""
+def create_sample_post(user, details=None):
+    """Create and return a sample post."""
+    payload = {
+        'title': 'Test Post',
+        'author': user,
+        'description': 'Test post description',
+        'body': 'Test post body',
+        'cover_image': 'path/to/image.jpg'
+    }
+    if details is not None:
+        payload.update(details)
+    return Post.objects.create(**payload)
+
+
+class UserModelTests(TestCase):
+    """Test for the User model."""
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
@@ -95,3 +115,89 @@ class ModelTest(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+
+class PostModelTests(TestCase):
+    """Tests for the models connected to the Post model."""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            email='userr@example.com',
+            password='test123',
+            first_name='Joe',
+            date_of_birth='2000-12-12'
+        )
+        self.post = create_sample_post(self.user)
+
+    def test_create_post(self):
+        """Test creating a post is successful."""
+        post = create_sample_post(self.user, {'title': 'Some title'})
+        self.assertEqual(str(post), post.title)
+
+    def test_slug_is_correct(self):
+        """Test slug is created correctly."""
+        title = 'Post title'
+        p = create_sample_post(self.user, {'title': title})
+        post = Post.objects.filter(title=title).first()
+        expected_slug = slugify(title)
+        self.assertEquals(expected_slug, post.slug)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        tag = Tag.objects.create(
+            user=self.user,
+            name='Tag1'
+        )
+
+        self.assertEqual(str(tag), tag.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_file_name_uuid(self, mock_uuid):
+        """Test generating image path."""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = image_file_path(None, 'image.jpg')
+
+        self.assertEqual(file_path, f'uploads/post/{uuid}.jpg')
+
+    def test_create_post_image(self):
+        """Test creating a post image is successful."""
+        post_image = PostImage.objects.create(
+            post=self.post,
+            title='image title',
+            image='path/to/image.jpg'
+        )
+
+        self.assertEqual(str(post_image), post_image.title)
+
+    def test_posts_images_property(self):
+        """Test images property of the Post model."""
+        post_image = PostImage.objects.create(
+            post=self.post,
+            title='image title',
+            image='path/to/image.jpg'
+        )
+
+        self.assertIn(post_image, self.post.images)
+        self.assertEqual(self.post.images[0], post_image)
+
+    def test_create_comment_is_successful(self):
+        """Test creating a comment is successful."""
+        comment = Comment.objects.create(
+            text='some comment',
+            author=self.user,
+            post=self.post,
+        )
+
+        self.assertEqual(str(comment), f'{comment.author}: {comment.text[:20]}')
+
+    def test_posts_comments_property(self):
+        """Test comments property of the Post model."""
+        comment = Comment.objects.create(
+            text='some comment',
+            author=self.user,
+            post=self.post,
+        )
+
+        self.assertIn(comment, self.post.comments)
+        self.assertEqual(self.post.comments[0], comment)
